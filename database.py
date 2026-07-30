23410af9fd20d5ba1229e05fd539a97c88e7c5c6
Lynn Mansour <lynnmansour2006@gmail.com>
2026-07-30 19:52:34 +0300

    day1

---
+++ database.py
@@ -0,0 +1,13 @@
+import sqlite3
+connection=sqlite3.connect("student.db")
+cursor=connection.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students(
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+age INTEGER,
+email TEXT)
+""")
+connection.commit
+connection.close
+print("database created")
---
+++ main.py
@@ -0,0 +1,4 @@
+import database
+print("=========================")
+print("Student management system")
+print("=========================")
